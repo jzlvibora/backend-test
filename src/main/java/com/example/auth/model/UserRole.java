f8e06681e1b39ccfb73356865de7dabc2fd997d2
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-22 15:47:59 +0800

    commentesd out greetings rest api and roles entity for testing

--- src/main/java/com/example/auth/model/UserRole.java
+++ src/main/java/com/example/auth/model/UserRole.java
@@ -2,41 +2,41 @@ package com.example.auth.model;
 
 import javax.persistence.*;
 
-@Table
-@Entity
+//@Table
+//@Entity
 public class  UserRole {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column
-    private String role;
-
-    @JoinColumn(name="user_id")
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private User user;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//    @Column
+//    private String role;
+//
+//    @JoinColumn(name="user_id")
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    private User user;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
 }
