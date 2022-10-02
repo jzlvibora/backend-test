b5b7dcd4ee5701e6d63e03c752631ecd5bcb854c
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-02 22:11:52 +0800

    added @JsonIgnore to Post and User fields

--- src/main/java/com/example/auth/model/Comment.java
+++ src/main/java/com/example/auth/model/Comment.java
@@ -1,5 +1,6 @@
 package com.example.auth.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,23 +8,26 @@ import lombok.NoArgsConstructor;
 
 import javax.annotation.processing.Generated;
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.Instant;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Comment {
+public class Comment implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
     private String text;
+    @JsonIgnore
     @ManyToOne(fetch= FetchType.LAZY)
     @JoinColumn(name="postId", referencedColumnName = "id")
     private Post post;
     private Long postIdentity;
     private Instant createdAt;
+    @JsonIgnore
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="userId", referencedColumnName = "id")
     private User user;
