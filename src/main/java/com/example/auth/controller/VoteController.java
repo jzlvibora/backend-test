eea86e0fad2c5be4b600e872ff161204d535f274
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-03 11:22:58 +0800

    added Crossorigin annotation

--- src/main/java/com/example/auth/controller/VoteController.java
+++ src/main/java/com/example/auth/controller/VoteController.java
@@ -5,11 +5,9 @@ import com.example.auth.service.VoteService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(value = "http://localhost:4200")
 @RestController
 @RequestMapping("/votes")
 @AllArgsConstructor
